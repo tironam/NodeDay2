61e3d7fe603534b67ef5d08629db831fc6063716
Michael Tirona <tironamichael@gmail.com>
2020-05-12 11:58:22 -0700

    Added code

--- app.js
+++ app.js
@@ -1,15 +1,80 @@
-const inquirer = require('inquirer')
+const { prompt } = require('inquirer')
 
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'example',
-    message: 'Type anything:'
-  }
-])
-  .then(data => {
-    console.log(data.example)
+const info = ['name', 'age', 'email', 'username', 'password', 'address', 'phone number', 'ssn']
+
+let questions = []
+for (let i = 0; i < info.length; i++) {
+  questions.push({
+  type: 'input',
+  name: info[i],
+  message: `What is your ${info[i]}?`
   })
-  .catch(err => console.log(err))
+}
 
-  
+prompt(questions)
+  .then(data => {
+    console.log(data)
+  })
+  .catch (err => console.log(err))
+
+// This won't work because of the asynchronisity 
+// for (let i =0; i< info.length; i++) {
+//  prompt({
+//    type: 'input',
+//    name: info[i],
+//    message: `What is your ${info[i]}?`
+//  })
+//  .then (data => {
+//    console.log(data)
+//  })
+//  .catch(err => console.log(err))
+// }
+
+
+// inquirer.prompt([
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: 'Name:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'age',
+//     message: 'Age:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'email',
+//     message: 'Email:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'username',
+//     message: 'Username:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'password',
+//     message: 'Password:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'address',
+//     message: 'Address:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'phone',
+//     message: 'Phone Number:'
+//   },
+//   {
+//     type: 'input',
+//     name: 'ssn',
+//     message: 'SSN:'
+//   }
+// ])
+//   .then(data => {
+//     console.log(data.example)
+//   })
+//   .catch(err => console.log(err))
+
